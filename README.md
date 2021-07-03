ca883cfc0f793808d8a1a5fa09fecb7cbce6429b
antoinemadec <ajamadec@gmail.com>
2021-07-03 10:55:31 +0200

    refactor: cleaner way to pass msg_filter

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # zulipbot
+
 ## Requirements
+
+### Python Packages
 ```python
 pip3 install -r requirements.txt
 ```
+### Setup Files
+Add the following files in this directory:
+  - **zuliprc**, used by Zulip's client. See [Zulip documentation](https://zulip.com/api/running-bots)
+  - **praw.ini**, used by Reddit's client. See [praw documentation](https://praw.readthedocs.io/en/latest/getting_started/configuration/prawini.html)
+  - **msg_filter.json**, used to filter Zulip messages the bot should respond to. See example below
 
-Add **zulip.rc** in this directory.
+```
+{
+    "type": "stream",
+    "display_recipient": "MyChannel"
+}
+```
 
-Add **praw.ini** in this directory
+## Usage
+```shell
+./main.py
+```
+Then, in Zulip, type:
+```
+!help
+```
 
 ## Acknowledgements
 Thanks to my buddy T3lchar for letting me still his bot: https://github.com/T3lchar/zulip_bot
--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python3
 
-from zulip import Client
+import json
+import sys
+import os.path
+
 import praw
+from zulip import Client
 
 from zulipbot.bot import ZulipBot
 from zulipbot.commands import *
 
-client = Client(config_file="./zuliprc")    # requires ./zuliprc
-reddit = praw.Reddit("bot1")                # requires ./praw.ini
 
-msg_filter = {
-    "type": "stream",
-    "display_recipient": "Bosch",
-}
-# msg_filter = {
-#     "type": "private"
-# }
+# --------------------------------------------------------------
+# functions
+# --------------------------------------------------------------
+def assert_file_exists(path):
+    if not os.path.isfile(path):
+        print("ERROR: file '{}' wasn't found\nSee README.md".format(path),
+              file=sys.stderr)
+        exit(1)
+
+
+# --------------------------------------------------------------
+# execution
+# --------------------------------------------------------------
+assert_file_exists("./zuliprc")
+assert_file_exists("./praw.ini")
+assert_file_exists("./msg_filter.json")
+client = Client(config_file="./zuliprc")
+reddit = praw.Reddit("bot1")
+with open('./msg_filter.json') as f:
+    msg_filter = json.load(f)
 
 bot = ZulipBot(client, msg_filter)
-
 bot.add_cmd(ZulipBotCmdCoucou())
 bot.add_cmd(ZulipBotCmdGnagnagna())
 bot.add_cmd(ZulipBotCmdJoke(reddit))
-
 bot.run()
