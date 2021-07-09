6984c3924c33d271512358915f3df979c4e3c6d1
antoinemadec <ajamadec@gmail.com>
2021-07-09 11:57:12 +0200

    feat(bot): fork cmd queues in background
    Commands are not blocking each other anymore.

--- zulipbot/bot.py
+++ zulipbot/bot.py
@@ -1,3 +1,5 @@
+from multiprocessing import Process, Queue
+
 import zulip
 
 from zulipbot.commands import *
@@ -11,21 +13,34 @@ class ZulipBot(object):
         self.client = client
         self.msg_filter = msg_filter
         self.cmds: list[ZulipBotCmdBase] = []
+        self.msg_queues: dict[str,Queue] = {}
         self.add_cmd(ZulipBotCmdHelp(self.cmds))
         self.add_cmd(ZulipBotCmdRepeat(self.cmds))
 
     def add_cmd(self, cmd: ZulipBotCmdBase):
         self.cmds.append(cmd)
+        self.msg_queues[cmd.cmd_name] = Queue()
+
+    def run_cmd(self, cmd: ZulipBotCmdBase, msg: ZulipMsg):
+        try:
+            cmd.process(msg)
+        except:
+            msg.reply(
+                f"{msg.cmd_prefix}{cmd.cmd_name} failed", is_error=True)
+
+    def process_queue(self, cmd: ZulipBotCmdBase):
+        while True:
+            msg = self.msg_queues[cmd.cmd_name].get()
+            self.run_cmd(cmd, msg)
 
     def run_callback(self, m):
         msg = ZulipMsg(self.client, self.msg_filter, m)
         for cmd in self.cmds:
             if cmd.is_to_be_processed(msg):
-                try:
-                    cmd.process(msg)
-                except:
-                    msg.reply(
-                        f"{msg.cmd_prefix}{cmd.cmd_name} failed", is_error=True)
+                self.msg_queues[cmd.cmd_name].put(msg)
 
     def run(self):
+        for cmd in self.cmds:
+            p = Process(target=self.process_queue, args=(cmd,))
+            p.start()
         self.client.call_on_each_message(self.run_callback)
